a0b52ff90d187645628a99fdc82ddd2e45f596a0
John Abrahams <jabrah23@gmail.com>
2019-10-01 11:26:33 -0400

    Fix some testing stuffs

--- tests/unit/routes/submission/workflow-test.js
+++ tests/unit/routes/submission/workflow-test.js
@@ -1,11 +1,11 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 
-module('Unit | Route | submission/new', function(hooks) {
+module('Unit | Route | submission/workflow', function(hooks) {
   setupTest(hooks);
 
   test('it exists', function(assert) {
-    let route = this.owner.lookup('route:submission/new');
+    let route = this.owner.lookup('route:submission/workflow');
     assert.ok(route);
   });
 });
