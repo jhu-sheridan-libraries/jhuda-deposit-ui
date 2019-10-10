43e46943980cee475807e89178fa0330416fda40
John Abrahams <jabrah23@gmail.com>
2019-10-10 11:35:50 -0400

    Add ember-models-table for the dashboard

---
+++ app/controllers/dashboard.js
@@ -0,0 +1,15 @@
+import Controller from '@ember/controller';
+// import { computed } from '@ember/object';
+
+export default Controller.extend({
+
+  // columns: computed(function () {
+  //   return [
+  //     {
+  //       propertyName: 'status',
+  //       title: 'Submission Name'
+  //     }
+  //   ];
+  // })
+
+});
--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -1,8 +1,21 @@
 import Route from '@ember/routing/route';
+import Bootstrap4Theme from 'ember-models-table/themes/bootstrap4';
 
 export default Route.extend({
   model() {
     // Load all submissions to be displayed here
     return this.store.query('submission', { user: '1' }, { include: 'files' });
+  },
+
+  setupController(controller) {
+    this._super(...arguments);
+    controller.set('columns', [
+      { propertyName: '_metadataJson.title', title: 'Submission Name', component: 'submission-title-cell' },
+      { propertyName: 'status', title: 'Submission Status' },
+      { propertyName: 'metadataStatus', title: 'Metadata Status' },
+      { propertyName: 'files.length', title: 'Files' },
+      { propertyName: 'filesStatus', title: 'Files Status' }
+    ]);
+    controller.set('themeInstance', Bootstrap4Theme.create());
   }
 });
