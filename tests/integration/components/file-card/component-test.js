4e9b1304bf963936f98e4985137b98935b011636
John Abrahams <jabrah23@gmail.com>
2019-11-11 15:41:18 -0500

    Add some unit tests

--- tests/integration/components/file-card/component-test.js
+++ tests/integration/components/file-card/component-test.js
@@ -1,26 +1,63 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, click, fillIn } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
+import EmberObject, { set } from '@ember/object';
+import { A } from '@ember/array';
 
 module('Integration | Component | file-card', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+  hooks.beforeEach(function () {
+    const file = EmberObject.create({
+      name: 'ThisIsAFile.moo',
+      description: 'The cows graze in the sun happily',
+      status: 'moo'
+    });
 
-    await render(hbs`<FileCard />`);
+    set(this, 'fileGrp', {
+      file
+    });
+  });
 
-    assert.equal(this.element.textContent.trim(), '');
+  test('Component displays file name, description, status, and required action message', async function (assert) {
+    set(this, 'fileGrp', {
+      ...this.fileGrp,
+      hasActions: true,
+      actions: A([
+        EmberObject.create({ type: 'file', key: 'ThisIsAFile.moo', details: 'Your Moo needs a little somethin' })
+      ])
+    });
+    await render(hbs`<FileCard @fileGrp={{fileGrp}}/>`);
 
-    // Template block usage:
-    await render(hbs`
-      <FileCard>
-        template block text
-      </FileCard>
-    `);
+    assert.dom('.card').includesText('The cows graze in the sun happily');
+    assert.dom('.card').includesText('ThisIsAFile.moo');
+    assert.dom('.card').includesText('Status: moo');
+    assert.dom('.card').includesText('Your Moo needs');
+  });
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+  test('Clicking "Show" button should let a user edit the description', async function (assert) {
+    set(this, 'editAction', () => {});
+
+    await render(hbs`<FileCard @fileGrp={{fileGrp}} @editAction={{editAction}} />`);
+
+    assert.dom('[data-test-file-edit-btn]').exists();
+    await click('[data-test-file-edit-btn]');
+
+    assert.dom('[data-test-description-editor]').exists();
+  });
+
+  test('Editing description changes description on File', async function (assert) {
+    set(this, 'editAction', () => {});
+
+    await render(hbs`<FileCard @fileGrp={{fileGrp}} @editAction={{editAction}} />`);
+    await click('[data-test-file-edit-btn]');
+
+    await fillIn('[data-test-description-editor]', 'The hip new moo');
+
+    await click('[data-test-save-filedesc-btn]');
+
+    const file = this.fileGrp.file;
+    assert.equal(file.description, 'The hip new moo');
   });
 });
