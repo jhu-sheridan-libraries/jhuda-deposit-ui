4c8b99456d434673461bb758d421af61a1aff328
John Abrahams <jabrah23@gmail.com>
2019-11-14 11:38:50 -0500

    Get 'delete' button working

--- app/components/file-card/component.js
+++ app/components/file-card/component.js
@@ -1,9 +1,17 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
-import { set } from '@ember/object';
+import { computed, set } from '@ember/object';
 import { alias } from '@ember/object/computed';
 
 export default class FileCardComponent extends Component {
+  // Named arguments passed into the component
+  fileGrp;
+  removeAction;
+  editAction;
+  editable;
+  index;
+
+  // Model aliases
   @alias('args.fileGrp.file') file;
   @alias('args.fileGrp.actions') actions;
   @alias('args.fileGrp.hasActions') hasActions;
@@ -11,19 +19,26 @@ export default class FileCardComponent extends Component {
   @tracked editing = false;
   tmpDescription;
 
+  @computed('index')
+  get noTop() {
+    return this.args.index > 0;
+  }
+
   toggleEditing() {
-    if (!this.editing) {
-      set(this, 'tmpDescription', this.file.description);
-    }
+    // if (!this.editing) {
+    //   set(this, 'tmpDescription', this.file.description);
+    // }
     set(this, 'editing', !this.editing);
   }
 
   saveMd() {
-    set(this, 'file.description', this.tmpDescription);
+    // set(this, 'file.description', this.tmpDescription);
+    this.file.save();
     this.toggleEditing();
   }
 
   cancelEdit() {
+    this.file.rollbackAttributes();
     this.toggleEditing();
   }
 }
--- mirage/config.js
+++ mirage/config.js
@@ -3,7 +3,10 @@ export default function() {
   this.get('/users/:id');
   this.get('/submissions');
   this.get('/submissions/:id');
+
   this.get('/files/:id');
+  this.patch('/files/:id');
+
   this.get('/submission-actions/:id');
 
 }
