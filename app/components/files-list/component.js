7249f82648617c4216263e8e7ac7d4579432f39a
John Abrahams <jabrah23@gmail.com>
2019-11-21 04:34:19 -0500

    Let user modify file metadata in the workflow for draft submission (#34)
    * Testing native classes again
    
    * Playing with components
    
    * Move buttons a bit. Stub an 'add' action
    
    * Show required actions in file list
    
    * Add some unit tests
    
    * Restructure files list
    
    * Get 'delete' button working
    
    * * Change some attribute refs
    * Delete File button works again
    * Fix some tests
    
    * Fix some tests
    
    * Try to do some cleanup
    
    * Update app/components/file-card/template.hbs
    
    Use `fn` helper to pass args
    
    Co-Authored-By: Jared Galanis <jaredgalanis@users.noreply.github.com>
    
    * Update app/components/file-card/template.hbs
    
    Use the `{{on}}` modifier instead of `{{action}}` helper
    
    Co-Authored-By: Jared Galanis <jaredgalanis@users.noreply.github.com>
    
    * Dependency update
    
    * Better Ember practices for FileCard
    
    * Glimmerify the FilesList component
    
    * Remove unnecessary comment

--- app/components/files-list/component.js
+++ app/components/files-list/component.js
@@ -1,47 +1,30 @@
-import Component from '@ember/component';
-import { computed, get } from '@ember/object';
+import Component from '@glimmer/component';
 import { A } from '@ember/array';
 
-export default Component.extend({
+/**
+ * Named properties passed to this component
+ *  - files
+ *  - submissionActions
+ *  - editable
+ *  - removeAction
+ */
+export default class FilesList extends Component {
 
   /**
    * Combine files with any associated actions so they can be displayed
    */
-  filesWithActions: computed(
-    'files',
-    'submissionActions',
-    'submissionActions.@each.{key,description}',
-    function () {
-      const actions = get(this, 'submissionActions') || A();
+  get filesWithActions() {
+    const actions = this.args.submissionActions || A();
 
-      return get(this, 'files').map((file) => {
-        const id = file.id;
-        const fileActions = actions.filter(a => a.key === id);
+    return this.args.files.map((file) => {
+      const id = file.id;
+      const fileActions = actions.filter(a => a.key === id);
 
-        return {
-          file,
-          hasActions: fileActions.length > 0,
-          actions: fileActions
-        };
-      });
-    }
-  ),
-
-  actions: {
-    /**
-     * Bubble the remove request up to parent component if `removeAction` is defined
-     * @param {File} file file obj
-     */
-    doRemove(file) {
-      this.removeAction(file);
-    },
-
-    /**
-     * Bubble the 'edit' request up to parent component if `editAction` is defined
-     * @param {File} file
-     */
-    doEdit(file) {
-      this.editAction(file);
-    }
+      return {
+        file,
+        hasActions: fileActions.length > 0,
+        actions: fileActions
+      };
+    });
   }
-});
+}
--- mirage/config.js
+++ mirage/config.js
@@ -3,7 +3,11 @@ export default function() {
   this.get('/users/:id');
   this.get('/submissions');
   this.get('/submissions/:id');
+
   this.get('/files/:id');
+  this.patch('/files/:id');
+  this.delete('/files/:id');
+
   this.get('/submission-actions/:id');
 
 }
